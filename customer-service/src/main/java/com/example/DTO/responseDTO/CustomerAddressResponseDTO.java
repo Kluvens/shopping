6793ede0c5855dc1b62eb2c301a8493ae89a304a
Kluvens <95273765+Kluvens@users.noreply.github.com>
2024-02-11 12:03:02 +1100

    Initial commit

---
+++ customer-service/src/main/java/com/example/DTO/responseDTO/CustomerAddressResponseDTO.java
@@ -0,0 +1,18 @@
+package com.example.DTO.responseDTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+public class CustomerAddressDTO {
+    private String id;
+    private String street;
+    private String suburb;
+    private String state;
+
+}
